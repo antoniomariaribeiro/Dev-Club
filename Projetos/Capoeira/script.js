f61a09006bdf9e4256c004f1b1a5d030765941fa
antoniomaria27 <antonio.ribeiro-pro@outlook.com>
2025-09-04 13:39:47 -0300

    Salvando no git Hub

---
+++ Projetos/Capoeira/script.js
@@ -0,0 +1,9 @@
+function visible() {
+    const form = document.querySelector('#formulario');
+    
+    if (form.style.display === "none" || form.style.display === "") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
